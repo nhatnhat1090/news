10703b753b2c10e337581a82188d4f0fe7f52a9b
tuandn6264 <tuandn6264@seta-asia.com.vn>
2015-12-14 16:15:21 +0700

    Initializing

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=cms-shop',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
